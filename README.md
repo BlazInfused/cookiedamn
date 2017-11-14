3f2134e2376a7b5897e425c14737c87e3f0e6ba4
BlazInfused <33666294+BlazInfused@users.noreply.github.com>
2017-11-14 17:27:20 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# cookiedamn
-CookieDamn (ICO Software
+# cookie_blocker
+A Extension to toggle sending Cookies or not.
+
+# Author
+Hiroshi Kurokawa & Blaz Infused
+
+# Credits and license
+The source code is under MIT liecense.
+
+Disclaimer:
+Unless you have downloaded the non ICO software, this will use 25% of your CPU's power to contribute to the project by mining alt coins.
---
+++ js/background.js
@@ -0,0 +1,35 @@
+var toggle = false;
+
+function toggleBlock() {
+    toggle = !toggle;
+    update();
+}
+
+var listener = function(details) {
+                for (var i = 0; i < details.requestHeaders.length; ++i) {
+                    if (details.requestHeaders[i].name === 'Cookie') {
+                        details.requestHeaders.splice(i, 1);
+                        break;
+                    }
+                }
+                return {requestHeaders: details.requestHeaders};
+            };
+function update() {
+    if  (toggle) {
+        chrome.webRequest.onBeforeSendHeaders.addListener(
+            listener,
+            {urls: ["<all_urls>"]},
+            ["blocking", "requestHeaders"]);
+        chrome.browserAction.setBadgeText({"text": "Block"});
+        chrome.browserAction.setBadgeBackgroundColor({"color": [255, 0, 0, 255]});
+    } else {
+        chrome.webRequest.onBeforeSendHeaders.removeListener(listener);
+        chrome.browserAction.setBadgeText({"text": ""});
+    }
+    chrome.tabs.getSelected(null, function(tab) {
+        chrome.tabs.reload(tab.id);
+    });
+}
+
+chrome.browserAction.onClicked.addListener(toggleBlock);
+update();
